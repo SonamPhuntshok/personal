6f947baddb0b9d854ff87d1971abd75248f77cd2
SonamPhuntshok <sonammmm2001@gmail.com>
2025-06-09 21:35:59 +0600

    personal

--- config/db.js
+++ config/db.js
@@ -1,16 +1,13 @@
 const pgp = require('pg-promise')();
 require('dotenv').config();
 
-// Connection string or object
 const db = pgp({
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 5432,
   database: process.env.DB_NAME || 'personal',
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || '10505000825',
-  ssl:{
-    rejectUnauthorized: false,
-  }
+  ssl: false // <- Disable SSL explicitly
 });
 
 module.exports = db;
