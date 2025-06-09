a022a48952ae978382496acb5e7e0be6e58f8b7e
SonamPhuntshok <sonammmm2001@gmail.com>
2025-06-09 21:03:49 +0600

    personal

--- config/db.js
+++ config/db.js
@@ -7,7 +7,10 @@ const db = pgp({
   port: process.env.DB_PORT || 5432,
   database: process.env.DB_NAME || 'personal',
   user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || '10505000825'
+  password: process.env.DB_PASSWORD || '10505000825',
+  ssl:{
+    rejectUnauthorized: false,
+  }
 });
 
 module.exports = db;
